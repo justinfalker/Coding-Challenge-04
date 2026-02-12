06de94948f14d253126d455ab48fb3d616a6a6dd
Justin Falker <falkerj@usf.edu>
2026-02-12 13:57:48 -0500

    Creation of products array

---
+++ cc_4.js
@@ -0,0 +1,7 @@
+const products = [
+  { name: "Airpods", category: "Electronics", price: 299.99, inventory: 18 },
+  { name: "Graphic T-Shirt", category: "Clothing", price: 35, inventory: 128 },
+  { name: "Oreos", category: "Food", price: 12.5, inventory: 33 },
+  { name: "tissues", category: "Household", price: 3.99, inventory: 30 },
+  { name: "Ballpoint Pens", category: "School/Office", price: 2.99, inventory: 200 }
+  ;
