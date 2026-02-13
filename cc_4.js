b8297ea7d88721eb6b2a1da624a43c4a5834704f
Justin Falker <falkerj@usf.edu>
2026-02-13 09:55:03 -0500

    Created for of loop

--- cc_4.js
+++ cc_4.js
@@ -4,4 +4,27 @@ const products = [
   { name: "Oreos", category: "Food", price: 12.5, inventory: 33 },
   { name: "tissues", category: "Household", price: 3.99, inventory: 30 },
   { name: "Ballpoint Pens", category: "School/Office", price: 2.99, inventory: 200 }
-  ;
+];
+
+  for (const p of products) {
+  let discountRate = 0;
+
+  switch (p.category) {
+    case "Electronics":
+      discountRate = 0.2;
+      break;
+    case "Clothing":
+      discountRate = 0.15;
+      break;
+    case "Food":
+    case "Household":
+      discountRate = 0.1;
+      break;
+    default:
+      console.log("No discount for you!!");
+  }
+
+  p.discountedPrice = p.price * (1 - discountRate);
+}
+
+console.log(products)
